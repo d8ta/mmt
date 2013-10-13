fc914b37e0d3ff6f9033fe9e130e5002b43d99f9
d8ta <danieltaggeselle@gmx.de>
2013-10-13 09:59:49 +0200

    bsp9 fertig

--- bsp09/bsp09/Program.cs
+++ bsp09/bsp09/Program.cs
@@ -20,19 +20,32 @@ namespace bsp09
 			int maximum = 255;
 
 			// a) for loop to go through the numbers and print it as ASCII
+			Console.WriteLine ("\n \n all ASCII letters are: ");
 			for (counter = minimum; counter <= maximum; counter ++) {
 				char symbol = (char)counter;
 				Console.WriteLine (counter.ToString() + " : " + symbol.ToString ());
 			}
 
 			// b) lowercase letters
+			Console.WriteLine ("\n \n all lowercase letters are: ");
+			for (counter = 97; counter <= 122; counter ++) {
+				char symbol = (char)counter;
+				Console.WriteLine (symbol.ToString ());
+			}
 		
 			// c) uppercase letters
+			Console.WriteLine ("\n \n all uppercase letters are: ");
+			for (counter = 65; counter <= 90; counter ++) {
+				char symbol = (char)counter;
+				Console.WriteLine (symbol.ToString ());
+				}
 
 			// d) numbers
-
-
-
+			Console.WriteLine ("\n \n all numbers are: ");
+			for (counter = 48; counter <= 57; counter ++) {
+				char symbol = (char)counter;
+				Console.WriteLine (symbol.ToString ());
+				}
 		}
 	}
 }
