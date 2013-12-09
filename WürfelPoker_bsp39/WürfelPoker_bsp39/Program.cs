a4cdcc8d969eb416efc53640e5c756fd64161a9b
d8ta <danieltaggeselle@gmx.de>
2013-12-09 20:54:56 +0100

    Würfelpoker fertig... bis auf exeptions, asserts und letzte bediengung

--- WürfelPoker_bsp39/WürfelPoker_bsp39/Program.cs
+++ WürfelPoker_bsp39/WürfelPoker_bsp39/Program.cs
@@ -4,8 +4,13 @@ namespace WürfelPoker_bsp39
 {
 	class MainClass
 	{
-		static int[] PlayerA = new int[9];
-		static int[] PlayerB = new int[9];
+		//static int[] PlayerA = new int[9];
+		//static int[] PlayerB = new int[9];
+
+		static int PlayerA = 0;
+		static int PlayerB = 0;
+		static int sumA = 0;
+		static int sumB= 0;
 
 		// Main
 		public static void Main (string[] args)
@@ -22,24 +27,58 @@ namespace WürfelPoker_bsp39
 			return random.Next (min, max);
 		} // Ende
 
-
 		// Spielrunde
 		static void PokerRounds ()
 		{
 			for (int i = 0; i < 10; i++) 
 			{
-				int codeA = RollingPlayerA ();
-				int codeB = RollingPlayerB ();
+				int caseA = RollingPlayerA ();
+				int caseB = RollingPlayerB ();
 				// If Conditions für (a) - (e)
-				Console.WriteLine (codeA);						
-			}
-		}
+				if (caseA == 1 && caseB == 1)
+				{
+					if (sumA > sumB) {
+						PlayerA += 1;
+					} else if (sumB > sumA) {
+						PlayerB += 1;
+					} else if (sumA == sumB) { // Wenn Gleiche Punktzahl dann bekommt keiner einen Punkt
+						PlayerB += 0;
+						PlayerA += 0;
+					}
+				} else if (caseA == 1 && caseB == 2 || caseB == 3) { 
+					PlayerB += 1;
+				} else if (caseB == 1 && caseA == 2 || caseA == 3) {
+					PlayerA += 1;
+				} else if (caseA == 2 && caseB == 2) {
+					if (sumA > sumB) {
+						PlayerA += 1;
+					} else if (sumB > sumA) {
+						PlayerB += 1;
+					} else if (sumA == sumB) { // Wenn Gleiche Punktzahl dann bekommt keiner einen Punkt
+						PlayerB += 0;
+						PlayerA += 0;
+					}
+				} else if (caseA == 2 && caseB == 3) {
+					PlayerB += 1;
+				} else if (caseB == 2 && caseA == 3) {
+					PlayerA += 1;
+				} else if (caseA == 3 && caseB == 3) {
+					if (sumA > sumB) {
+						PlayerA += 1;
+					} else if (sumB > sumA) {
+						PlayerB += 1;
+					} else if (sumA == sumB) { // Wenn Gleiche Punktzahl dann bekommt keiner einen Punkt
+					}
+				} 
+				Console.WriteLine ("Player A has " + PlayerA + " Points\n" +
+					"Player B has " + PlayerB + " Points.");
+				if (PlayerA > PlayerB) {
 
-		// Vergleiche A und B
-		static int ComparePlayerRolls ()
-		{
-			int leer = 0;
-			return leer;
+					Console.WriteLine ("Congradulations PlayerA, you won!");
+				} else {
+					Console.WriteLine ("Congradulations PlayerB, you won!");
+				}
+			}
 		}
 
 
@@ -54,27 +93,22 @@ namespace WürfelPoker_bsp39
 			int rolling3 = RandomGenerator (1, 6);
 			// Fall (a) drei verschiedene Augenzahlen
 			if (rolling1 != rolling2 && rolling1 != rolling3 && rolling2 != rolling3) {
-				/*Console.WriteLine (rolling1);
-				Console.WriteLine (rolling2);
-				Console.WriteLine (rolling3);
-				Console.WriteLine ("\n three different cards");*/
+				Console.WriteLine ("\nA three different cards");
 				resultA = 1;
+				Console.WriteLine(sumA = rolling1 + rolling2 + rolling3);
 			}
 			// Fall (b) ein Paar
 			else if ((rolling1 == rolling2 && rolling1 !=rolling3) || (rolling1 == rolling3 && rolling1 != rolling2) || (rolling2 == rolling3 && rolling2 != rolling1)) {
-				/*	Console.WriteLine (rolling1);
-				Console.WriteLine (rolling2);
-				Console.WriteLine (rolling3);
-				Console.WriteLine ("\n a pair");*/
+				Console.WriteLine ("\nA a pair");
 				resultA = 2;
+				Console.WriteLine(sumA = rolling1 + rolling2 + rolling3);
 			}
 			// Fall (d) ein Triple
 			else if (rolling1 == rolling2 && rolling1 == rolling3 && rolling2 == rolling3) {
-				/*	Console.WriteLine (rolling1);
-				Console.WriteLine (rolling2);
-				Console.WriteLine (rolling3);
-				Console.WriteLine ("\n a triple");*/
+
+				Console.WriteLine ("\nA a triple");
 				resultA = 3;
+				Console.WriteLine(sumA = rolling1 + rolling2 + rolling3);
 			} 
 			return resultA;
 
@@ -90,30 +124,23 @@ namespace WürfelPoker_bsp39
 			int rolling3 = RandomGenerator (1, 6);
 			// Fall (a) drei verschiedene Augenzahlen
 			if (rolling1 != rolling2 && rolling1 != rolling3 && rolling2 != rolling3) {
-				/*	Console.WriteLine (rolling1);
-				Console.WriteLine (rolling2);
-				Console.WriteLine (rolling3);
-				Console.WriteLine ("\ndifferent cards");*/
+				Console.WriteLine ("\nB three different cards");
 				resultB = 1;
+				Console.WriteLine(sumB = rolling1 + rolling2 + rolling3);
 			}
 			// Fall (b) ein Paar
 			else if ((rolling1 == rolling2 && rolling1 !=rolling3) || (rolling1 == rolling3 && rolling1 != rolling2) || (rolling2 == rolling3 && rolling2 != rolling1)) {
-				/*	Console.WriteLine (rolling1);
-				Console.WriteLine (rolling2);
-				Console.WriteLine (rolling3);
-				Console.WriteLine ("\n a pair");*/
+				Console.WriteLine ("\nB a pair");
 				resultB = 2;
+				Console.WriteLine(sumB = rolling1 + rolling2 + rolling3);
 			}
 			// Fall (d) ein Triple
 			else if (rolling1 == rolling2 && rolling1 == rolling3 && rolling2 == rolling3) {
-				/*	Console.WriteLine (rolling1);
-				Console.WriteLine (rolling2);
-				Console.WriteLine (rolling3);
-				Console.WriteLine ("\n a triple");*/
+				Console.WriteLine ("\nB a triple");
 				resultB = 3;
+				Console.WriteLine(sumB = rolling1 + rolling2 + rolling3);
 			} 
 			return resultB;
-
 		} // Ende PlayerB
 	}
 }
